0f30ae23b8afdba6f6df316f8baa62ab7961bbb5
shangdanning <1350148930@qq.com>
2018-11-13 16:00:51 +0800

    添加剩余文件

---
+++ js/JD.js
@@ -0,0 +1,9 @@
+﻿var html = document.querySelector('html');
+changeRem();
+window.addEventListener('resize', changeRem);
+function changeRem() {
+    var width = html.getBoundingClientRect().width;
+    html.style.fontSize = width / 10 + 'px';
+}
+
+
